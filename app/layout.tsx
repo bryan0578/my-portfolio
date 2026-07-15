3e49b14966815229908233ad2a06697461e05ef6
Bryan Cash <bryan.cash78@gmail.com>
2026-07-15 18:55:03 -0400

    Polish Dylan proposal on mobile
    * Add mobile polish for Dylan proposal
    
    * Load proposal mobile polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/lib/seo/site"
 import { SITE_DESCRIPTION } from "@/lib/seo/brand-copy"
 import "./globals.css"
+import "./proposal-mobile.css"
 
 const defaultDescription = SITE_DESCRIPTION
 
